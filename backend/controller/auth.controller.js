0a73f8a1e852f7a1e905aff299f2b31126d95813
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-14 13:47:08 +0530

    feat: add authentication controller with signup, login, and logout functions

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,11 @@
+export const signupController = async(req, res) => {
+    res.send('signup');
+}
+
+export const loginController = async(req, res) => {
+    res.send('login');
+}
+
+export const logoutController = async(req, res) => {
+    res.send('logout');
+}
