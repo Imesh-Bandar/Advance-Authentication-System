b0ff1585d7b51bf0264fa3be1bb5e29bf374a743
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-14 23:15:19 +0530

    feat: add welcome email functionality and fix formatting in email sending logic

--- backend/mail/email.js
+++ backend/mail/email.js
@@ -1,5 +1,5 @@
 import { MailTrapClient, sender } from "./mailtrap.config.js"
-import { mailTemplates } from "./mailTemplate.js";
+import { mailTemplates, welcomeTemplate } from "./mailTemplate.js";
 
 const { verificationTemplate } = mailTemplates;
 
@@ -20,6 +20,28 @@ export const sendVerificationEmail = async (email, token) => {
         console.log("Verification email sent successfully:", response);
     } catch (error) {
         console.error("Error sending verification email:", error);
+
+    }
+
+}
+
+//=========================|| Send Welcome Email ||=========================//
+export const sendWelcomeEmail=async (email, username) => {
+    const recipient = [{
+        email: email,
+    }]
+    try {
+        const response= await MailTrapClient.send({
+            from:sender,
+            to:recipient,
+            subject:"Welcome to Advance Authentication System",
+            html: welcomeTemplate({ username }), // <-- Add comma here
+            category:"Welcome Email"
+        })
+        console.log("Welcome email sent successfully:", response);
+    } catch (error) {
+        console.error("Error sending welcome email:", error);
+
         
     }
 
