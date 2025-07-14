1d9fae4c0191b12fb8141db7bc893e2f8f0f2c31
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-14 22:28:05 +0530

    feat: implement user signup functionality with validation and email verification

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,11 +1,78 @@
-export const signupController = async(req, res) => {
-    res.send('signup');
-}
+import User from "../model/user.model.js";
+import bcryptjs from "bcryptjs";
+import { genarateVerificationToken } from "../utils/genarateVerificationToken.js";
+import { genarateTokenandSetCookie } from "../utils/genarateTokenandSetCookie.js";
+import { sendVerificationEmail } from "../mail/email.js";
+//controller for user signup
+export const signupController = async (req, res) => {
+  //get user data from request body
+  const { username, email, password } = req.body;
+  try {
+    //check if all fields are provided
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    } else {
+      //==================|| check if email is already exit ||==================//
+      //check if user already exists
+      const userAlreadyExists = await User.findOne({ email });
 
-export const loginController = async(req, res) => {
-    res.send('login');
-}
+      //if user already exists, return error
+      if (userAlreadyExists) {
+        return res
+          .status(400)
+          .json({ status: "success", message: "Email already exists" });
+      } else {
 
-export const logoutController = async(req, res) => {
-    res.send('logout');
-}
+
+
+        //==================|| create new user ||==================//
+        
+        //hashpassword
+        const hashpassword = await bcryptjs.hash(password, 10);
+        //generate verification token
+        const verificationToken = await genarateVerificationToken();
+        //create new user
+        const newUser = new User({
+          username,
+          email,
+          password: hashpassword,
+          verificationToken,
+          verificationExpires: Date.now() + 24 * 60 * 60 * 1000, // Token valid for 24 hours
+        });
+        //save user to database
+        const user = await newUser.save();
+        //genarate token and cookie
+        genarateTokenandSetCookie(res, user._id);
+        // Send verification email
+        await sendVerificationEmail(user.email, verificationToken);
+
+        //return success response
+        return res.status(201).json({
+          status: "success",
+          message: "User created successfully",
+          user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            isVerified: user.isVerified,
+            verificationExpires: user.verificationExpires,
+            verificationToken: user.verificationToken,
+            createdAt: user.createdAt,
+          },
+        });
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    // Handle any unexpected errors
+    return res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
+};
+
+export const loginController = async (req, res) => {
+  res.send("login");
+};
+
+export const logoutController = async (req, res) => {
+  res.send("logout");
+};
